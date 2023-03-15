9f54ed5f2b16947528355db5c0ec4346679d3ff6
syaketaro <a.tomm722@icloud.com>
2023-03-15 16:19:04 +0900

    c

--- src/main/java/com/techacademy/controller/UserController.java
+++ src/main/java/com/techacademy/controller/UserController.java
@@ -39,33 +39,50 @@ public class UserController {
 		return "user/register";
 	}
 	
-	@PostMapping("/register")
-	public String postRegister(@Validated User user, BindingResult res, Model model) {
-		if(res.hasErrors()) {
-			
-			return getRegister(user);
-		}
-	
-	service.saveUser(user);
-	
-	return "redirect:/user/list";
-	}
-	
+	 @PostMapping("/register")
+	    public String postRegister(@Validated User user, BindingResult res, Model model) {
+	        if(res.hasErrors()) {
+	            // エラーあり
+	            return getRegister(user);
+	        }
+	        // User登録
+	        service.saveUser(user);
+	        // 一覧画面にリダイレクト
+	        return "redirect:/user/list";
+	    }
+	// User一覧画面から画面遷移した時とUser更新画面で入力チェックエラーがあった場合の遷移の違いがポイントになります。
+	/* User一覧から遷移してきた場合は、                    URLのパスパラメータにユーザーのIDが設定されているので、そこから取得できると思いますが、
+	 更新時にエラーが発生してUser更新画面を再表示する場合には  ユーザーのIDをUserクラスの中からIDを取得したいです。
+	 今の状態だとエラー発生時にgetUserを呼び出していますが、IDがわかる情報を渡していないのではないでしょうか？ */
 	@GetMapping("/update/{id}/")
-	public String getUser(@PathVariable("id")Integer id, Model model) {
-		
-		model.addAttribute("user", service.getUser(id));
-		
-		return "user/update";
-	}
+	 public String getUser(User user, @PathVariable("id") Integer id, Model model) {
+   //public String getUser(@PathVariable("id") Integer id, Model model) {	
+        // Modelに登録
+		if(id != null) {
+			model.addAttribute("user", service.getUser(id));
+		} 
+		else if(id == null) {
+			model.addAttribute("user", user);
+		//第二引数で実行しているのがpostUserメソッドを呼び出し直している状態ですね。なので、正しくセットできていないと言えます。ヒントの部分をそのまま素直に記述してみるとどうなりそうでしょうかね？
+		// ModelにはpostUser()から渡された引数のuserをセットする
+			
+		}
+        // User更新画面に遷移
+        return "user/update";
+    }
 	
 	@PostMapping("/update/{id}/")
-	public String postUser(User user) {
-		
+    public String postUser(Integer id, @Validated User user , BindingResult res, Model model) {
+  //public String postUser(User user) {	
+		if(res.hasErrors()) {
+			
+			return getUser(user, null, model);
+		}
+		// User登録
 		service.saveUser(user);
-		
-		return "redirect:/user/list";
-	}
+        // 一覧画面にリダイレクト
+        return "redirect:/user/list";
+    }
 	
 	@PostMapping(path="list", params="deleteRun")
 	public String deleteRun(@RequestParam(name="idck")Set<Integer> idck, Model model) {
